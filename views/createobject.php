4909693c0448162da9bcd9388c40fe241be7c365
gobbe05 <92081333+gobbe05@users.noreply.github.com>
2024-01-23 21:36:29 +0000

    Changed enctype on form

--- views/createobject.php
+++ views/createobject.php
@@ -18,7 +18,7 @@ $mysqli -> close();
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
-<form class="d-flex flex-column gap-1" action="/admin/createobject" method="post">
+<form class="d-flex flex-column gap-1" action="/admin/createobject" method="post" enctype="multipart/form-data">
   <h3>Create Object</h3>  
   <div class="form-floating">
         <input class="form-control" type="text" name="objekt" id="objekt">
